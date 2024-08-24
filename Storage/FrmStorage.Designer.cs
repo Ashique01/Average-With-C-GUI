539d6019855b439c66e71b63ba23e4ddbf86392d
Ashique01 <ashiquemura@gmail.com>
2024-08-24 19:41:54 +0600

    preoject commited

---
+++ Storage/FrmStorage.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Storage
+{
+    partial class FrmStorage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(83, 77);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnInfo.TabIndex = 0;
+            this.btnInfo.Text = "Go";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(66, 124);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(106, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check Button";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // FrmStorage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnInfo);
+            this.Name = "FrmStorage";
+            this.Text = "Storage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ Storage/FrmStorage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Storage
+{
+    public partial class FrmStorage : Form
+    {
+
+        int age;
+        string name;
+        double salary;
+
+        public FrmStorage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            
+
+            age = 25;
+            name = "Ashique";
+            salary = 25000.45;
+
+            MessageBox.Show("Your name is: " +name);
+            MessageBox.Show("Your age is: " + age.ToString());
+            MessageBox.Show("Your salary is: " + salary.ToString());
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your name is: " + name);
+        }
+    }
+}
